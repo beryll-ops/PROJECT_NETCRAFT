d6ac80deb52eefe7824eef9d76480577686af1d1
NathanielWolfgang <natanielwilczak076@gmail.com>
2025-11-27 21:11:28 +0100

    Online players count
    online players count -- pictures and prototype of functions

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var serwer_check = 2;
+var players_number_check = 0;
 
-function serwer_start() {
+function serwer_start() {                   //function for serwer start/stop
     if (serwer_check % 2 == 0) {
         document.getElementById('button_on_off').src="./resources/OFF.png";
         serwer_check += 1;
@@ -9,6 +10,7 @@ function serwer_start() {
         serwer_check += 1;
     }
 }
+
 const burgerIcon = document.querySelector('.burger-icon');
 const nav = document.querySelector('nav');
 const mainContent = document.querySelector('main');
@@ -22,3 +24,17 @@ function openMenu() {
 }
 
 burgerIcon.addEventListener('click', openMenu);
+
+function players_number() {                     //function showing number of players online
+    if (players_number_check <=9){
+        players_number_check += 1;
+        document.getElementById('number_0').src=`./resources/number_of_players_${players_number_check}.png`;
+    }   else {
+        test_stop();
+    }
+    
+}
+function test_stop() {                          //Test function -- should be deleted soon
+    players_number_check = 0;
+    document.getElementById('number_0').src=`./resources/number_of_players_${players_number_check}.png`;
+}
