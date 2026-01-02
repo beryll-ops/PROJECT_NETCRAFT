23062e77d19a77a8662e08695261d9c5dfcdc604
beryll-ops <75722271+beryll-ops@users.noreply.github.com>
2026-01-02 15:23:29 +0100

    Backend development started
    Node.js ready file structure
    modified .gitignore
    added environment variables
    
    To get access to server-side code contact owner

--- public/main.js
+++ public/main.js
@@ -2,9 +2,10 @@ const burgerIcon = document.querySelector('.burger-icon');
 const nav = document.querySelector('nav');
 const mainContent = document.querySelector('main');
 const footer = document.querySelector('footer');
-let i = 0;
+
+// let i = 0; ????????????
+
 function openMenu() {
-	// this function adds .open class to main menu and the burger icon when clicked, to open/close the menu,  then toggles the visibility of main content and footer. The if statement adds a 'closing' class only when menu is actually being closed, else removes it when menu is opened so the background is visible.
 	nav.classList.toggle('open');
 	burgerIcon.classList.toggle('open');
 	mainContent.classList.toggle('invisible');
@@ -18,11 +19,11 @@ function openMenu() {
 	}
 }
 burgerIcon.addEventListener('click', openMenu);
+
+
 // Flap clock script
 const targetDate = new Date('2024-12-31T23:59:59');
 function getTimeSegmentElements(segmentElement) {
-	// this function retrieves and returns the various elements of a time segment needed for the flap clock animation. Time segment element is a single digit.
-	// each element is selected using its class name within the provided segmentElement.
 	const segmentDisplay = segmentElement.querySelector('.segment-display');
 	const segmentDisplayTop = segmentDisplay.querySelector(
 		'.segment-display-top'
@@ -34,7 +35,6 @@ function getTimeSegmentElements(segmentElement) {
 	const overlayTop = segmentOverlay.querySelector('.segment-overlay-top');
 	const overlayBottom = segmentOverlay.querySelector('.segment-overlay-bottom');
 	return {
-		// return makes it possible to access these elements outside the function
 		segmentDisplayTop,
 		segmentDisplayBottom,
 		segmentOverlay,
@@ -42,23 +42,26 @@ function getTimeSegmentElements(segmentElement) {
 		overlayBottom,
 	};
 }
+
+
 function updateFlapClock(segmentElement, overlayElement, value) {
-	// this function updates the text content of the segment display and overlay elements with the provided value. (value of the before and after the flip)
 	segmentElement.textContent = value;
 	overlayElement.textContent = value;
 }
 
+
 function updateTimeSegment(segmentElement, timeValue) {
 	const segmentElements = getTimeSegmentElements(segmentElement);
-	// starts the flip animation by adding the 'flip' class to the segment overlay element
-	segmentElements.segmentOverlay.classList.add('flip');
-	updateFlapClock(
-		// updates the top part of the segment display and bottom part of the overlay with the new time value before the flip animation starts
-		segmentElements.segmentDisplayTop,
-		// possible error i don't remember if i changed overlayBottom to segmentOverlayBottom
-		segmentElements.overlayBottom,
-		timeValue
-	);
+	if (segmentElements.segmentDisplayTop.textContent == timeValue) {
+		return;
+	} else {
+		segmentElements.segmentOverlay.classList.add('flip');
+		updateFlapClock(
+			segmentElements.segmentDisplayTop,
+			segmentElements.overlayBottom,
+			timeValue
+		);
+	}
 	function finishAnimation() {
 		// function evokes when flip animation ends, removes class, updates bottom part of segment display and top part of overlay with new time value
 		segmentElements.segmentOverlay.classList.remove('flip');
@@ -76,6 +79,7 @@ function updateTimeSegment(segmentElement, timeValue) {
 	);
 }
 
+
 function updateTimeSection(sectionID, timeValue) {
 	//14
 	const firstNumber = Math.floor(timeValue / 10); //1
@@ -87,3 +91,34 @@ function updateTimeSection(sectionID, timeValue) {
 	updateTimeSegment(timeSegments[0], firstNumber);
 	updateTimeSegment(timeSegments[1], secondNumber);
 }
+
+
+let acapulco = 12432;
+async function fetchServerUptime() {
+	return acapulco; // Example uptime in seconds
+}
+
+
+setInterval(async () => {
+	await fetchServerUptime();
+	acapulco++;
+	console.log('Server uptime fetched' + acapulco);
+}, 1000);
+
+
+async function updateFlapClockDisplay() {
+	const uptimeInSeconds = await fetchServerUptime();
+	const totalSeconds = uptimeInSeconds;
+	const days = Math.floor(totalSeconds / 86400);
+	const hours = Math.floor((totalSeconds % 86400) / 3600);
+	const minutes = Math.floor((totalSeconds % 3600) / 60);
+	// const minutes = Math.floor((totalSeconds % 60) / 1);
+	updateTimeSection('days', days);
+	updateTimeSection('hours', hours);
+	updateTimeSection('minutes', minutes);
+}
+
+
+setInterval(async () => {
+	await updateFlapClockDisplay();
+}, 1000);
